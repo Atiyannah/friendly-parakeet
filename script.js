28effc8718e26e71f0be60b0a4afe50b8054c512
Atiyannah J <atiyannahjean-baptiste@Atiyannahs-MacBook-Air.local>
2022-05-24 22:18:51 -0400

    variables

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// Assignment code here
+/// Assignment code here
+var numbers = "1234567890";
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = "!?%$&";
 
+var passwordLength = [];
+
+var promptPassword = window.prompt("How long would you like your password to be? Choose a number between 8 and 27.");
+// link to passwordLength variable and move to after eventListener function
+
+// continue for rest of variables and test
+var numbersConfirm = window.confirm ("Would you like your password to have NUMBERS?");
+  if (numbersConfirm) {
+    for (var i = 0; i < numbers.length; i++);
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
